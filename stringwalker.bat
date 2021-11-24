c59abb0c59258135de0b9736b8649e166cf10ec9
sintrode <562c3fb8@opayq.com>
2021-11-24 16:58:20 -0500

    Aborts if %limit% goes past the end of %string%
    Script no longer returns empty strings when returning character 6 of a five-character string, for example.

--- stringwalker.bat
+++ stringwalker.bat
@@ -22,19 +22,19 @@ set /p "limit=Enter the one-indexed character to stop at: "
 set "%string%=throw an error if the string is empty" %two%>nul || exit /b
 set "%limit%=throw an error if the limit is empty" %two%>nul || exit /b
 
-echo String: %string%
-echo Stop at char #: %limit%
-
 :: call a subroutine and set the exit /b value to %current_index%/%limit%
 set counter=%zero%
 :loop
+setlocal
 call :get_substring || exit /b
+endlocal
 set /a counter+=one
 goto :loop
 
 :get_substring
 set /a index_label=counter+one
 call set "char=%%string:~%counter%,%one%%%"
+set "%char%=throw an error if we've gone past the end of the string" %two%>nul || exit /b %one%
 echo %index_label%:"%char%"
 set /a exit_code=%index_label%/%limit%
 exit /b %exit_code%
